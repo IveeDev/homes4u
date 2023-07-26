3647ff0d21880e0d735bac9b6a0cf79d18e27f83
IveeDev <iviidev899@gmail.com>
2023-07-26 16:12:24 +0100

    Created the navbar.

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,7 +1,50 @@
-import React from "react";
-
+import { NavLink } from "react-router-dom";
+import "./NavBar.scss";
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="nav">
+      <div className="container">
+        <div className="logo">
+          <NavLink to={"/"} className="link">
+            Homes4u
+          </NavLink>
+        </div>
+
+        <div className="nav__bar">
+          <ul>
+            <li className="nav__item">
+              <NavLink to="/" className="nav__link">
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav__item">
+              <NavLink to="/" className="nav__link">
+                Rent
+              </NavLink>
+            </li>
+
+            <li className="nav__item">
+              <NavLink to="/" className="nav__link">
+                Post Your Rentals
+              </NavLink>
+            </li>
+
+            <li className="nav__item">
+              <NavLink to="/" className="nav__link">
+                About us
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className="nav__btns">
+          <button className="btn btn--login">Login</button>
+          <button className="btn btn--signup">Sign up</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
