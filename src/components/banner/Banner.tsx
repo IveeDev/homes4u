d8161440d199e88bbfe329c6a1bfa2fa8ea93f54
IveeDev <iviidev899@gmail.com>
2023-07-26 18:22:21 +0100

    Create the search input form

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Banner.scss";
 import banner from "../../assets/img/banner.png";
+import SearchInput from "../searchInput/SearchInput";
 
 const Banner = () => {
   return (
     <section className="banner">
       <div className="container">
         <img src={banner} alt="banner" className="container__img" />
+        <div className="container__searchbar">
+          <SearchInput />
+        </div>
       </div>
     </section>
   );
